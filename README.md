f4dd20a791c90b3ec0be5e5bccc2856d1382d094
Peter Roome <pete@lostmy.name>
2016-08-01 22:53:06 +0100

    Some nicer styling with an arrow from the dropdown.

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ A menubar app for searching Giphy
 ## Menu Bar
 https://github.com/maxogden/menubar
 
+## Install stuff
+`npm install`
 
 ## Start the app
 `npm run start`
--- main.js
+++ main.js
@@ -1,57 +1,27 @@
 'use strict';
 
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+let path = require('path')
 
 require('electron-reload')(__dirname);
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
-
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 620, height: 800})
-
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+// Menubar
+const menubar = require('menubar')
+let menu = menubar({
+  tooltip: "Search Giphy?",
+  width: 420,
+  height: 600,
+  alwaysOnTop: true,
+  transparent: true,
+  frame: false,
+  icon: path.join(__dirname, 'public/assets/images', 'Icon.png'),
+  y: 20
 })
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+menu.on('ready', function ready(){
+  console.log('Ready…');
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+menu.on('after-create-window', function after(){
+  console.log('after-create-window.');
+  menu.window.openDevTools()
+})
