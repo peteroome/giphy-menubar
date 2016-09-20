7b98268f3489109ce4ed2ad42cefe880ae7a57aa
Peter Roome <pete@lostmy.name>
2016-09-20 21:34:03 +0100

    Tidy some masonry

--- views/components/search-results.js
+++ views/components/search-results.js
@@ -4,33 +4,17 @@ import SearchResultsLineItem from './search-results-line-item';
 
 class SearchResults extends React.Component {
   componentDidUpdate() {
-    let gifs = this.props.gifs;
-    var msnry = new Masonry('.gif-list', {
+    new Masonry('.gif-list', {
       itemSelector: 'li.gif',
       columnWidth: 200
     });
   }
 
   render() {
-    // let oddGifs  = [];
-    // let evenGifs = [];
-    let gifs = this.props.gifs;
-
-    // Pop the last gif so we have 24, not 25.
-    // 25 into two cols will never be equal.
-    // gifs.pop();
-    // gifs.filter((gif, index) => {
-    //   if((index + 1) % 2 === 0){
-    //     evenGifs.push(gif);
-    //   } else {
-    //     oddGifs.push(gif);
-    //   }
-    // });
-
     return(
       <div className="gif-list-container">
         <ul className='gif-list'>
-          {gifs.map((gif, index) => {
+          {this.props.gifs.map((gif, index) => {
             return(
               <SearchResultsLineItem key={index} gif={gif} />
             );
